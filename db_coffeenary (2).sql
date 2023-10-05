666945e074642118e91dee4dbde0622625325645
Nadini Annisa Byant <89978510+nadinibyant@users.noreply.github.com>
2023-10-05 14:34:07 +0700

    Add files via upload

---
+++ db_coffeenary (2).sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Okt 2023 pada 09.33
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_coffeenary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `meja`
+--
+
+CREATE TABLE `meja` (
+  `id_meja` int(11) NOT NULL,
+  `nomor_meja` int(11) NOT NULL,
+  `foto_meja` varchar(255) NOT NULL,
+  `jumlah_kursi` int(3) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `meja`
+--
+
+INSERT INTO `meja` (`id_meja`, `nomor_meja`, `foto_meja`, `jumlah_kursi`, `created_at`, `updated_at`) VALUES
+(1, 11, 'Hero Section.jpg', 41, '2023-10-01 05:45:53', '2023-10-01 07:50:38'),
+(3, 10, 'logomadani.png', 4, '2023-10-01 06:40:19', '2023-10-01 06:40:19');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reservasi`
+--
+
+CREATE TABLE `reservasi` (
+  `id_reservasi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_meja` int(11) NOT NULL,
+  `tanggal_reservasi` date NOT NULL,
+  `jam_mulai` time NOT NULL,
+  `jam_selesai` time NOT NULL,
+  `jumlah_orang` int(3) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `keterangan` longtext DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `full_name`, `username`, `password`, `email`, `no_telp`, `foto`, `created_at`, `updated_at`) VALUES
+(1, 'nadini annisa byant', 'dindini', '$2b$10$XFHiOJGK606vd3tO4Y5KJOEkl1G/.x7WzsgP1lGOS1Ef9dzMkGBji', 'nadini123@gmail.com', '32423432', 'Hero Section.jpg', '2023-09-30 15:14:25', '2023-10-01 10:35:38'),
+(2, 'nadini', 'dini aja', '$2b$10$sU6kNLBcE8IT2Ihs9srtXuT8MnHBnWTEq.DXC4C.6sQppSQS43WLW', 'nadini12@gmail.com', '324234326', NULL, '2023-09-30 15:16:29', '2023-09-30 15:16:29');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`id_meja`);
+
+--
+-- Indeks untuk tabel `reservasi`
+--
+ALTER TABLE `reservasi`
+  ADD PRIMARY KEY (`id_reservasi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_meja` (`id_meja`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `id_meja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `reservasi`
+--
+ALTER TABLE `reservasi`
+  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `reservasi`
+--
+ALTER TABLE `reservasi`
+  ADD CONSTRAINT `reservasi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservasi_ibfk_2` FOREIGN KEY (`id_meja`) REFERENCES `meja` (`id_meja`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
