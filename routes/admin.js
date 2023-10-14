6417f122578f68d534568db1eca5b8c472083d39
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2023-10-14 22:32:58 +0700

    update

--- routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controllers = require('../controllers/admin')
-
-router.get('/admin-reservation-history', controllers.manageReservationHistory)
-router.get('/reservation-history', controllers.allReservationData)
-
-module.exports = router
