2cd8413e5bb42d2a54e5f92859ed10e4701b8ebc
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2023-10-22 13:48:47 +0700

    update

--- routes/users/profile.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controllers = require('../../controllers/users/profile')
-
-router.get('/profileView', controllers.profileView)
-
-module.exports = router
